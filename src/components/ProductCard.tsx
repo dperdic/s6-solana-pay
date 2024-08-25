21f316b5dd101c154b5b5fff8631fde9a1609a34
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-25 20:39:51 +0200

    add to cart button

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { Product } from "@/store/store";
+import { Product, useCartStore, useTransactionStateStore } from "@/store/store";
 
 export default function ProductCard({ product }: { product: Product }) {
+  const addToCart = useCartStore((state) => state.addToCart);
+  const transactionInProgress = useTransactionStateStore(
+    (state) => state.transactionInProgress
+  );
+
   return (
     <div className="w-full flex flex-col border rounded-md gap-4 p-4 shadow-sm">
       <div>
@@ -22,6 +27,15 @@ export default function ProductCard({ product }: { product: Product }) {
         &nbsp;
         <span>{product.price} SOL</span>
       </div>
+
+      <button
+        type="button"
+        className="btn btn-md btn-black"
+        disabled={transactionInProgress || product.quantity <= 0}
+        onClick={() => addToCart(product)}
+      >
+        Add to cart
+      </button>
     </div>
   );
 }
