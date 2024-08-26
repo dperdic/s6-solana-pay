2439b85b886c6cea8f0cc96396807f88dbd97495
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-26 13:18:51 +0200

    cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import CartItem from "@/components/CartItem";
 import { useCartStore, useTransactionStateStore } from "@/store/store";
 
 export default function Cart() {
@@ -19,39 +18,19 @@ export default function Cart() {
       <h3 className="text-xl font-semibold mb-4">Cart</h3>
 
       <div className="flex flex-col p-4 bg-white rounded-md shadow w-full overflow-hidden overflow-x-auto">
-        {/* {cart.length ? (
-          <>
-            <div className="divide-y-2 divide-gray-200">
-              {cart.map((product) => (
-                <CartItem key={product.id} product={product} />
-              ))}
-            </div>
-            <div className="flex flex-col sm:flex-row gap-4 justify-between px-4">
-              <button
-                type="button"
-                className="btn btn-md btn-black"
-                disabled={transactionInProgress || !cart.length}
-                onClick={() => {}}
-              >
-                Buy
-              </button>
-
-              <span className="font-semibold">
-                Total: {totalPrice.toFixed(9)} SOL
-              </span>
-            </div>
-          </>
-        ) : (
-          <div className="text-center">Cart is empty</div>
-        )} */}
-
         <table className="min-w-full table-fixed">
           {!!cart.length && (
             <thead>
               <tr>
-                <th>Product</th>
-                <th>Quantity</th>
-                <th>Price</th>
+                <th className="px-4 py-2 text-xs font-semibold uppercase tracking-wider text-left w-5/12">
+                  Product
+                </th>
+                <th className="px-4 py-2 text-xs font-semibold uppercase tracking-wider text-center w-2/12">
+                  Quantity
+                </th>
+                <th className="px-4 py-2 text-xs font-semibold uppercase tracking-wider text-right w-5/12">
+                  Price
+                </th>
               </tr>
             </thead>
           )}
@@ -63,6 +42,7 @@ export default function Cart() {
                     <td className="px-4 py-2 text-sm font-normal text-gray-900 whitespace-pre-wrap text-left">
                       {product.name}
                     </td>
+
                     <td className="px-4 py-2 text-sm font-normal text-gray-900 whitespace-pre-wrap text-center">
                       <div className="flex gap-2 justify-center">
                         <button
@@ -102,6 +82,7 @@ export default function Cart() {
                         </button>
                       </div>
                     </td>
+
                     <td className="px-4 py-2 text-sm font-normal text-gray-900 whitespace-nowrap text-right tabular-nums">
                       {(product.quantity * product.price).toFixed(9)} SOL
                     </td>
@@ -111,10 +92,11 @@ export default function Cart() {
                 <tr>
                   <th
                     colSpan={2}
-                    className="px-4 py-2 text-sm text-gray-900 whitespace-pre-wrap text-left"
+                    className="px-4 py-2 text-xs font-semibold uppercase tracking-wider text-left"
                   >
                     Total price:
                   </th>
+
                   <td
                     colSpan={1}
                     className="px-4 py-2 text-sm font-normal text-gray-900 whitespace-nowrap text-right tabular-nums"
@@ -124,8 +106,15 @@ export default function Cart() {
                 </tr>
 
                 <tr>
-                  <td colSpan={9999} className="text-center">
-                    <button>Buy</button>
+                  <td colSpan={9999} className="text-center py-2">
+                    <button
+                      type="button"
+                      className="btn btn-md btn-black"
+                      disabled={transactionInProgress || !cart.length}
+                      onClick={() => {}}
+                    >
+                      Buy
+                    </button>
                   </td>
                 </tr>
               </>
